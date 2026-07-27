68fe720df07cf67cdda5415199a98bd60ac148e0
Omar Valencia <omarrutiliovalenciaborja@gmail.com>
2026-07-26 18:18:04 -0600

    algunos commits

--- backend/database/migrations/0000_01_01_000000_create_users_table.php
+++ backend/database/migrations/0000_01_01_000000_create_users_table.php
@@ -6,22 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
-{
-    Schema::create('users', function (Blueprint $table) {
-        $table->id();
-        $table->string('name');
-        $table->string('email')->unique();
-        $table->timestamp('email_verified_at')->nullable();
-        $table->string('password');
-        // Llave foránea para el rol
-        $table->foreignId('role_id')->constrained('roles')->onDelete('restrict');
-        $table->rememberToken();
-        $table->timestamps();
-    });
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->foreignId('role_id')->constrained('roles')->restrictOnDelete();
+            $table->rememberToken();
+            $table->timestamps();
+        });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
@@ -39,13 +35,10 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('users');
     }
 };
