2f3d9b0eea60a3ac0de81a1f59c48a1812c4f0fb
Omar Valencia <omarrutiliovalenciaborja@gmail.com>
2026-07-28 20:31:27 -0600

    Corrige carga de peliculas y centra login Google

--- backend/app/Models/ResenaReaccion.php
+++ backend/app/Models/ResenaReaccion.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ResenaReaccion extends Model
 {
+    protected $table = 'resena_reacciones';
+
     protected $fillable = ['resena_id', 'usuario_id', 'tipo'];
 }
